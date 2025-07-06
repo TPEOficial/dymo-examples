f6f544ffdef36366e65464339fb30d7beed72afb
FJRG2007 <fjrg2007@tpeoficial.com>
2025-07-06 02:01:18 +0200

    ✨ adding the new examples

---
+++ dymo-api/nodejs-sdk/basic-signup-verification.ts
@@ -0,0 +1,38 @@
+import DymoAPI from "dymo-api";
+
+const dymo = new DymoAPI({
+    apiKey: "PRIVATE_TOKEN_HERE"
+});
+
+async function checkSignUp({
+    email,
+    phone
+}: {
+    email: string;
+    phone: string;
+}): Promise<{
+    pass: boolean;
+    message?: string;
+    realEmail?: string;
+}> {
+    try {
+        const response = await dymo.isValidData({
+            email,  // User's email address.
+            phone   // If requested by the user (recommended).
+        });
+
+        // Email cheks.
+        if (!response.email.valid) return { pass: false, message: "Use a valid email." };
+        if (response.email.fraud) return { pass: false, message: "Use your real email." };
+        if (response.email.proxiedEmail) return { pass: false, message: "Use your real email." };
+
+        // Phone cheks.
+        if (!response.phone.valid) return { pass: false, message: "Phone is not valid." };
+        if (response.phone.fraud) return { pass: false, message: "Use your real phone." };
+
+        return { pass: true, realEmail: response.email.email };
+    } catch (error) {
+        console.error(error);
+        return { pass: true };
+    }
+};
---
+++ dymo-api/php-sdk/basic-signup-verification.php
@@ -0,0 +1,33 @@
+<?php
+
+require "vendor/autoload.php";
+
+use TPEOficial\DymoAPI;
+
+function checkSignUp(string $email, string $phone): array {
+    $dymoClient = new DymoAPI([
+        "api_key" => "PRIVATE_TOKEN_HERE"
+    ]);
+
+    try {
+        $response = $dymoClient->isValidData([
+            "email" => $email,  // User's email address.
+            "phone" => $phone   // If requested by the user (recommended).
+        ]);
+
+        // Email checks.
+        if (!$response->email->valid) return ["pass" => false, "message" => "Use a valid email."];
+        if ($response->email->fraud) return ["pass" => false, "message" => "Use your real email."];
+        if ($response->email->proxiedEmail) return ["pass" => false, "message" => "Use your real email."];
+        if ($response->email->freeSubdomain) return ["pass" => false, "message" => "Use your real email."];
+
+        // Phone checks.
+        if (!$response->phone->valid) return ["pass" => false, "message" => "Phone is not valid."];
+        if ($response->phone->fraud) return ["pass" => false, "message" => "Use your real phone."];
+
+        return ["pass" => true, "realEmail" => $response->email->email];
+    } catch (Exception $e) {
+        error_log("Error during signup check: " . $e->getMessage());
+        return ["pass" => true];
+    }
+}
